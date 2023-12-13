e83b0514e247a7f44508623664eac1d3680f4599
abdurrozaqf <rozaqa27@gmail.com>
2023-12-13 21:44:32 +0700

    feat: add layout components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from "react";
+
+import { Toaster } from "@/components/ui/toaster";
+import Navbar from "./Navbar";
+
+import SidebarUser from "@/components/SidebarUser";
+import SidebarAdmin from "@/components/SidebarAdmin";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Layout = (props: Readonly<Props>) => {
+  const { children } = props;
+
+  return (
+    <div className="w-full h-screen flex flex-col font-inter transition-all overflow-auto">
+      <Navbar />
+      <div className="flex grow">
+        <SidebarUser />
+        {/* <SidebarAdmin /> */}
+
+        {/* CONTENT */}
+        <div className="grow overflow-auto flex flex-col p-6 bg-[#E4ECF1]">
+          {children}
+        </div>
+      </div>
+      <Toaster />
+    </div>
+  );
+};
+
+export default Layout;
