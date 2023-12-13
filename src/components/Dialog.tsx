292bad43a998099c66e1690edec578f32eb2d2f0
abdurrozaqf <rozaqa27@gmail.com>
2023-12-13 21:43:34 +0700

    feat: add custom alert dialog components

---
+++ src/components/Dialog.tsx
@@ -0,0 +1,43 @@
+import { ReactNode } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+type Props = {
+  title?: string;
+  children: ReactNode;
+  description?: string | any;
+  onCancel?: () => {};
+  onAction?: () => {};
+  action?: string;
+};
+
+const CustomDialog = (props: Props) => {
+  const { children, title, description, onCancel, onAction, action } = props;
+
+  return (
+    <Dialog>
+      <DialogTrigger>{children}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          {onCancel && <Button onClick={onCancel}>Cancel</Button>}
+          {onAction && <Button onClick={onAction}>{action}</Button>}
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CustomDialog;
