9bceb2ad668475d9805e3419da87fbdceffffa0c
abdurrozaqf <rozaqa27@gmail.com>
2023-12-14 22:37:19 +0700

    feat: add type product

---
+++ src/utils/apis/products/types.ts
@@ -0,0 +1,15 @@
+export type Product = {
+  productID: 1;
+  name: string;
+  cpu: string;
+  ram: string;
+  display: string;
+  storage: string;
+  thickness: string;
+  weight: string;
+  bluetooth: string;
+  hdmi: string;
+  price: string;
+  category: string;
+  image: string;
+};
