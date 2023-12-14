e83f9d3ce169f9c6c6db71ff9131a4ff49a37d1f
abdurrozaqf <rozaqa27@gmail.com>
2023-12-14 22:29:30 +0700

    fix: sidebar components

--- src/components/SidebarUser.tsx
+++ src/components/SidebarUser.tsx
@@ -1,87 +1,211 @@
 import {
   BoxIcon,
+  ChevronFirstIcon,
   ChevronLastIcon,
   GitCompareArrowsIcon,
   HomeIcon,
   LogInIcon,
+  LogOutIcon,
   MoonStarIcon,
   SunIcon,
 } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Checkbox } from "@/components/ui/checkbox";
+// import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
 
 import { useTheme } from "@/utils/contexts/theme-provider";
-import { Button } from "./ui/button";
 
-const SidebarUser = () => {
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+interface Props {
+  handleChange?: (event: any) => void;
+}
+
+const SidebarUser = (props: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { pathname } = useLocation();
+  const { handleChange } = props;
+  const navigate = useNavigate();
+
   const { setTheme, theme } = useTheme();
   function handleTheme() {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
   }
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <div className="font-poppins">
       {/* SIDEBAR */}
-      <div className="h-full p-12 border-r relative flex flex-col justify-between shadow-md">
+      <div
+        className={
+          isOpen
+            ? `h-full px-12 py-6 border-r relative flex flex-col justify-between shadow-md transition-all duration-300`
+            : `h-full px-6 py-6 border-r relative flex flex-col justify-start shadow-md transition-all duration-300`
+        }
+      >
         {/* BUTTON SIDEBAR */}
-        <div className="bg-[#E4ECF1] dark:bg-[#1265AE] shadow-md p-2 rounded-md absolute right-4 cursor-pointer">
-          <ChevronLastIcon />
+        <div
+          className={
+            isOpen
+              ? `p-2 bg-[#E4ECF1] dark:bg-[#1265AE] hover:bg-[#1265AE] hover:dark:bg-[#E4ECF1] hover:text-white hover:dark:text-black w-fit h-fit rounded-lg shadow-md cursor-pointer absolute right-4`
+              : `p-2 bg-[#E4ECF1] dark:bg-[#1265AE] hover:bg-[#1265AE] hover:dark:bg-[#E4ECF1] hover:text-white hover:dark:text-black w-fit h-fit rounded-lg shadow-md cursor-pointer mb-10`
+          }
+          onClick={toggle}
+        >
+          {isOpen ? <ChevronFirstIcon /> : <ChevronLastIcon />}
         </div>
 
-        {/* BUTTON HOME */}
-        <div className="flex flex-col gap-4">
-          <div className="flex items-center gap-2 cursor-pointer">
-            <div className="p-2 bg-[#E4ECF1] dark:bg-[#1265AE] w-fit h-fit rounded-lg shadow-md">
+        {/* BUTTON MENU */}
+        <div
+          className={
+            isOpen ? `flex flex-col gap-4` : `flex flex-col gap-4 grow`
+          }
+        >
+          <div className="flex items-center gap-2">
+            <div
+              className="p-2 bg-[#E4ECF1] dark:bg-[#1265AE] hover:bg-[#1265AE] hover:dark:bg-[#E4ECF1] hover:text-white hover:dark:text-black w-fit h-fit rounded-lg shadow-md cursor-pointer"
+              onClick={() => navigate("/")}
+            >
               <HomeIcon />
             </div>
-            <p className="w-40 hover:bg-[#E4ECF1] dark:hover:bg-white rounded-md p-2 dark:hover:text-black">
+            <p
+              className={
+                isOpen
+                  ? `w-40 font-medium translate-x-0 opacity-100 transition-all cursor-pointer hover:bg-[#E4ECF1] dark:hover:bg-white rounded-md p-2 dark:hover:text-black`
+                  : `-translate-x-28 opacity-0 transition-all absolute`
+              }
+              onClick={() => navigate("/")}
+            >
               Home
             </p>
           </div>
-          <div className="flex items-center gap-2 cursor-pointer">
-            <div className="p-2 bg-[#E4ECF1] dark:bg-[#1265AE] w-fit h-fit rounded-lg shadow-md">
+          <div className="flex items-center gap-2">
+            <div
+              className="p-2 bg-[#E4ECF1] dark:bg-[#1265AE] hover:bg-[#1265AE] hover:dark:bg-[#E4ECF1] hover:text-white hover:dark:text-black w-fit h-fit rounded-lg shadow-md cursor-pointer"
+              onClick={() => navigate("/products")}
+            >
               <BoxIcon />
             </div>
-            <p className="w-40 hover:bg-[#E4ECF1] dark:hover:bg-white rounded-md p-2 dark:hover:text-black">
+            <p
+              className={
+                isOpen
+                  ? `w-40 font-medium translate-x-0 opacity-100 transition-all cursor-pointer hover:bg-[#E4ECF1] dark:hover:bg-white rounded-md p-2 dark:hover:text-black`
+                  : `-translate-x-28 opacity-0 transition-all absolute`
+              }
+              onClick={() => navigate("/products")}
+            >
               Products
             </p>
           </div>
           <div className="flex items-center gap-2 cursor-pointer">
-            <div className="p-2 bg-[#E4ECF1] dark:bg-[#1265AE] w-fit h-fit rounded-lg shadow-md">
+            <div className="p-2 bg-[#E4ECF1] dark:bg-[#1265AE] hover:bg-[#1265AE] hover:dark:bg-[#E4ECF1] hover:text-white hover:dark:text-black w-fit h-fit rounded-lg shadow-md cursor-pointer">
               <GitCompareArrowsIcon />
             </div>
-            <p className="w-40 hover:bg-[#E4ECF1] dark:hover:bg-white rounded-md p-2 dark:hover:text-black">
+            <p
+              className={
+                isOpen
+                  ? `w-40 font-medium translate-x-0 opacity-100 transition-all cursor-pointer hover:bg-[#E4ECF1] dark:hover:bg-white rounded-md p-2 dark:hover:text-black`
+                  : `-translate-x-28 opacity-0 transition-all absolute`
+              }
+            >
               Compare
             </p>
           </div>
         </div>
 
         {/* CATEGORIES */}
-        <div className="flex flex-col gap-4">
-          <h1>Categories:</h1>
-          <div className="flex items-center gap-2 ml-4">
-            <Checkbox id="terms" />
+
+        <div
+          className={
+            isOpen
+              ? `translate-x-0 flex flex-col gap-4 opacity-100 transition-all`
+              : `-translate-x-28 w-0 opacity-0 transition-all`
+          }
+        >
+          <h1 className="font-medium">Categories:</h1>
+          <div
+            className="flex items-center gap-2 ml-4"
+            onClick={() => navigate("/products")}
+          >
+            {/* <Checkbox id="all" value="" onChange={handleChange} /> */}
+            <input
+              onChange={handleChange}
+              type="radio"
+              value=""
+              id="all"
+              name="category"
+              disabled={pathname == "/"}
+              className="cursor-pointer"
+            />
             <label
-              htmlFor="terms"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              htmlFor="all"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer"
+            >
+              All
+            </label>
+          </div>
+          <div
+            className="flex items-center gap-2 ml-4"
+            onClick={() => navigate("/products")}
+          >
+            {/* <Checkbox id="office" value="office" onChange={handleChange} /> */}
+            <input
+              onChange={handleChange}
+              type="radio"
+              value="office"
+              id="office"
+              name="category"
+              disabled={pathname == "/"}
+              className="cursor-pointer"
+            />
+            <label
+              htmlFor="office"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer"
             >
               Office
             </label>
           </div>
-          <div className="flex items-center gap-2 ml-4">
-            <Checkbox id="terms" />
+          <div
+            className="flex items-center gap-2 ml-4"
+            onClick={() => navigate("/products")}
+          >
+            {/* <Checkbox id="multimedia" /> */}
+            <input
+              onChange={handleChange}
+              type="radio"
+              value="multimedia"
+              id="multimedia"
+              name="category"
+              disabled={pathname == "/"}
+              className="cursor-pointer"
+            />
             <label
-              htmlFor="terms"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              htmlFor="multimedia"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer"
             >
               Multimedia
             </label>
           </div>
-          <div className="flex items-center gap-2 ml-4">
-            <Checkbox id="terms" />
+          <div
+            className="flex items-center gap-2 ml-4"
+            onClick={() => navigate("/products")}
+          >
+            {/* <Checkbox id="gaming" /> */}
+            <input
+              onChange={handleChange}
+              type="radio"
+              value="gaming"
+              id="gaming"
+              name="category"
+              disabled={pathname == "/"}
+              className="cursor-pointer"
+            />
             <label
-              htmlFor="terms"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              htmlFor="gaming"
+              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer"
             >
               Gaming
             </label>
@@ -89,8 +213,14 @@ const SidebarUser = () => {
         </div>
 
         {/* RANGE ON BUDGET */}
-        <div className="flex flex-col gap-4">
-          <h1>Range on budget :</h1>
+        <div
+          className={
+            isOpen
+              ? `translate-x-0 w-fit flex flex-col gap-4 opacity-100 transition-all`
+              : `-translate-x-28 w-0 opacity-0 transition-all`
+          }
+        >
+          <h1 className="font-medium">Range on budget :</h1>
           <form className="flex flex-col gap-2">
             <input
               type="text"
@@ -116,7 +246,14 @@ const SidebarUser = () => {
               <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-            <p className="font-medium flex flex-col">
+
+            <p
+              className={
+                isOpen
+                  ? `font-medium w-full opacity-100 translate-x-0 transition-all rounded-md p-2 dark:hover:text-black flex flex-col`
+                  : `w-0 opacity-0 -translate-x-28 transition-all absolute`
+              }
+            >
               <span>John Doe</span>
               <span className="text-xs">johndoe@mail.com</span>
             </p>
@@ -125,24 +262,53 @@ const SidebarUser = () => {
             onClick={() => handleTheme()}
             className="flex items-center gap-2 cursor-pointer "
           >
-            <div className="p-2 rounded-md shadow-md w-fit dark:bg-[#1265AE] transition-opacity">
+            <div className="p-2 rounded-md shadow-md bg-[#E4ECF1] dark:bg-[#1265AE] hover:bg-[#1265AE] hover:dark:bg-[#E4ECF1] hover:text-white hover:dark:text-black">
               {theme === "light" ? <SunIcon /> : <MoonStarIcon />}
             </div>
-            <p className="w-full hover:bg-[#E4ECF1] dark:hover:bg-white rounded-md p-2 dark:hover:text-black">
-              Change theme
+            <p
+              className={
+                isOpen
+                  ? `font-medium w-full opacity-100 translate-x-0 transition-all hover:bg-[#E4ECF1] dark:hover:bg-white rounded-md p-2 dark:hover:text-black`
+                  : `w-0 opacity-0 -translate-x-28 transition-all absolute`
+              }
+            >
+              Change Theme
             </p>
           </div>
-          <div className="flex items-center gap-2">
+          <div
+            className="flex items-center gap-2 cursor-pointer"
+            onClick={() => navigate("/login")}
+          >
             <div className="p-2 rounded-md shadow-md w-fit bg-[#48B774]">
               <LogInIcon color="white" />
             </div>
-            <p>Login</p>
+            <p
+              className={
+                isOpen
+                  ? `font-medium w-full opacity-100 translate-x-0 transition-all hover:bg-[#E4ECF1] dark:hover:bg-white rounded-md p-2 dark:hover:text-black`
+                  : `w-0 opacity-0 -translate-x-28 transition-all absolute`
+              }
+            >
+              Login
+            </p>
           </div>
-          <div className="flex items-center gap-2">
+          <div
+            className="flex items-center gap-2 cursor-pointer"
+            // onClick={() => changeToken()}
+          >
             <div className="p-2 rounded-md shadow-md w-fit bg-[#FF5858]">
-              <LogInIcon color="white" />
+              <LogOutIcon color="white" />
             </div>
-            <p>Logout</p>
+
+            <p
+              className={
+                isOpen
+                  ? `font-medium w-full opacity-100 translate-x-0 transition-all hover:bg-[#E4ECF1] dark:hover:bg-white rounded-md p-2 dark:hover:text-black`
+                  : `w-0 opacity-0 -translate-x-28 transition-all absolute`
+              }
+            >
+              Logout
+            </p>
           </div>
         </div>
       </div>
