e2e91a0c84d94999b950bc8a35ae0229f68cd545
abdurrozaqf <rozaqa27@gmail.com>
2023-12-15 19:24:25 +0700

    feat: add type schema for form validation in admin

---
+++ src/utils/apis/admin/index.ts
@@ -0,0 +1,12 @@
+import {
+  AddProductSchema,
+  EditProductSchema,
+  StatusSchema,
+  Transactions,
+  addProductSchema,
+  editProductSchema,
+  statusSchema,
+} from "./types";
+
+export { addProductSchema, editProductSchema, statusSchema };
+export type { Transactions, AddProductSchema, EditProductSchema, StatusSchema };
