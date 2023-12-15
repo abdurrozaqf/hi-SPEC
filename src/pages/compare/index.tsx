a767de80796aa0ccb8af96030316743d7c1999d2
gufranlazuardi <gufranlazuardi@gmail.com>
2023-12-15 16:55:56 +0700

    feat: add compare page

--- src/pages/compare/index.tsx
+++ src/pages/compare/index.tsx
@@ -1,12 +1,166 @@
 import Layout from "@/components/Layout";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft, Search, X } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
+import { TypeProducts, productsSampleData } from "./sampleDataCompare";
+import CardCompare from "@/components/CardCompare";
 
 const Compare = () => {
+  const [query1, setQuery1] = useState("");
+  const [query2, setQuery2] = useState("");
+  const [query3, setQuery3] = useState("");
+
+  const handleInputChange1 = (event: any) => {
+    setQuery1(event.target.value);
+  };
+  const handleInputChange2 = (event: any) => {
+    setQuery2(event.target.value);
+  };
+  const handleInputChange3 = (event: any) => {
+    setQuery3(event.target.value);
+  };
+
+  const filteredItems1 = productsSampleData.filter(
+    (product) => product.name.toLowerCase().indexOf(query1.toLowerCase()) !== -1
+  );
+  const filteredItems2 = productsSampleData.filter(
+    (product) => product.name.toLowerCase().indexOf(query2.toLowerCase()) !== -1
+  );
+  const filteredItems3 = productsSampleData.filter(
+    (product) => product.name.toLowerCase().indexOf(query3.toLowerCase()) !== -1
+  );
+
+  function filteredData1(datas: TypeProducts[], query: string) {
+    let filteredProducts = datas;
+
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems1;
+    }
+
+    return filteredProducts.map(
+      ({
+        name,
+        cpu,
+        ram,
+        display,
+        storage,
+        thickness,
+        weight,
+        bluetooth,
+        hdmi,
+        price,
+        category,
+        image,
+      }) => (
+        <CardCompare
+          key={Math.random()}
+          name={name}
+          cpu={cpu}
+          ram={ram}
+          display={display}
+          storage={storage}
+          thickness={thickness}
+          weight={weight}
+          bluetooth={bluetooth}
+          hdmi={hdmi}
+          price={price}
+          category={category}
+          image={image}
+        />
+      )
+    );
+  }
+  function filteredData2(datas: TypeProducts[], query: string) {
+    let filteredProducts = datas;
+
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems2;
+    }
+
+    return filteredProducts.map(
+      ({
+        name,
+        cpu,
+        ram,
+        display,
+        storage,
+        thickness,
+        weight,
+        bluetooth,
+        hdmi,
+        price,
+        category,
+        image,
+      }) => (
+        <CardCompare
+          key={Math.random()}
+          name={name}
+          cpu={cpu}
+          ram={ram}
+          display={display}
+          storage={storage}
+          thickness={thickness}
+          weight={weight}
+          bluetooth={bluetooth}
+          hdmi={hdmi}
+          price={price}
+          category={category}
+          image={image}
+        />
+      )
+    );
+  }
+  function filteredData3(datas: TypeProducts[], query: string) {
+    let filteredProducts = datas;
+
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems3;
+    }
+
+    return filteredProducts.map(
+      ({
+        name,
+        cpu,
+        ram,
+        display,
+        storage,
+        thickness,
+        weight,
+        bluetooth,
+        hdmi,
+        price,
+        category,
+        image,
+      }) => (
+        <CardCompare
+          key={Math.random()}
+          name={name}
+          cpu={cpu}
+          ram={ram}
+          display={display}
+          storage={storage}
+          thickness={thickness}
+          weight={weight}
+          bluetooth={bluetooth}
+          hdmi={hdmi}
+          price={price}
+          category={category}
+          image={image}
+        />
+      )
+    );
+  }
+
+  const result1 = filteredData1(productsSampleData, query1);
+  const result2 = filteredData2(productsSampleData, query2);
+  const result3 = filteredData3(productsSampleData, query3);
+
   return (
     <Layout>
-      <div className="grow bg-white shadow-lg rounded-xl  p-32 font-poppins">
+      <div className="grow bg-white dark:bg-transparent shadow-lg rounded-xl  p-32 font-poppins">
         <div className="flex items-center mb-10">
           <button className="flex">
             <div className="mr-4">
@@ -17,98 +171,70 @@ const Compare = () => {
           <h1 className=" grow text-center text-4xl font-bold">Compare</h1>
         </div>
         <div className="grid gap-6 grid-cols-3">
-          <div className=" h-full   border rounded-md p-8">
+          <div className=" h-fit border rounded-md p-8">
             <div className="flex justify-between">
-              <div className="flex gap-8">
+              <div className="flex w-full">
                 <div>
                   <Search />
                 </div>
-                <p className="">Asus ROG Strix</p>
+                <input
+                  onChange={(e) => setQuery1(e.target.value)}
+                  type="text"
+                  placeholder="Asus ROG Strix"
+                  className=" outline-none grow mx-4 dark:bg-transparent "
+                />
               </div>
-              <div className=" justify-end">
+              <div className="">
                 <X />
               </div>
             </div>
-            <img
-              src="https://www.asus.com/media/Odin/Websites/global/ProductLine/20200824120842.jpg?webp"
-              alt="ROG Strix"
-            />
-            <div className="flex flex-col text-center gap-2 pt-4 pb-8">
-              <p className=" font-bold text-2xl">Asus ROG Strix</p>
-              <p className=" font-bold text-3xl">Rp 40.000.000</p>
-            </div>
-            <div className=" mt-4 flex flex-col gap-8">
-              <p>CPU : Intel Core i9</p>
-              <p>RAM : dfasf</p>
-              <p>Display : dfasf</p>
-              <p>Storage : dfasf</p>
-              <p>Thickness : dfasf</p>
-              <p>Weight : dfasf</p>
-              <p>Bluetooth : dfasf</p>
-              <p>HDMI : dfasf</p>
-            </div>
+
+            {/* manggil function */}
+            {query1 && <>{filteredData1(productsSampleData, query1)}</>}
           </div>
-          <div className=" h-full  border rounded-md p-8">
+
+          <div className="  h-fit border rounded-md p-8">
             <div className="flex justify-between">
-              <div className="flex gap-8">
+              <div className="flex w-full">
                 <div>
                   <Search />
                 </div>
-                <p className=" ">Asus ROG Zephyrus</p>
+                <input
+                  onChange={(e) => setQuery2(e.target.value)}
+                  type="text"
+                  placeholder="Asus ROG Strix"
+                  className=" outline-none grow mx-4 dark:bg-transparent "
+                />
               </div>
-              <div className=" justify-end">
+              <div className="">
                 <X />
               </div>
             </div>
-            <img
-              src="https://www.asus.com/media/Odin/Websites/global/ProductLine/20200824120814.jpg?webp"
-              alt="ROG Zephyrus"
-            />
-            <div className="flex flex-col text-center gap-2 pt-4 pb-8">
-              <p className=" font-bold text-2xl">Asus ROG Zephyrus</p>
-              <p className=" font-bold text-3xl">Rp 40.000.000</p>
-            </div>
-            <div className=" flex flex-col gap-8 mt-4">
-              <p>CPU : Intel Core i9</p>
-              <p>RAM : dfasf</p>
-              <p>Display : dfasf</p>
-              <p>Storage : dfasf</p>
-              <p>Thickness : dfasf</p>
-              <p>Weight : dfasf</p>
-              <p>Bluetooth : dfasf</p>
-              <p>HDMI : dfasf</p>
-            </div>
+
+            {/* manggil function */}
+            {query2 && <>{filteredData2(productsSampleData, query2)}</>}
           </div>
-          <div className=" h-full  border rounded-md p-8">
+
+          <div className=" h-fit border rounded-md p-8">
             <div className="flex justify-between">
-              <div className="flex gap-8">
+              <div className="flex w-full">
                 <div>
                   <Search />
                 </div>
-                <p className="">Asus ROG Flow</p>
+                <input
+                  onChange={(e) => setQuery3(e.target.value)}
+                  type="text"
+                  placeholder="Asus ROG Strix"
+                  className=" outline-none grow mx-4 dark:bg-transparent "
+                />
               </div>
-              <div className=" justify-end">
+              <div className="">
                 <X />
               </div>
             </div>
-            <img
-              src="https://www.asus.com/media/Odin/Websites/global/ProductLine/20200824120546.jpg?webp"
-              alt="ROG Flow"
-            />
-            <div className="flex flex-col text-center gap-2 pt-4 pb-8">
-              <p className=" font-bold text-2xl">Asus ROG Flow</p>
-              <p className=" font-bold text-3xl">Rp 40.000.000</p>
-            </div>
-            <div className=" flex flex-col gap-8 mt-4">
-              <p>CPU : Intel Core i9</p>
-              <p>RAM : dfasf</p>
-              <p>Display : dfasf</p>
-              <p>Storage : dfasf</p>
-              <p>Thickness : dfasf</p>
-              <p>Weight : dfasf</p>
-              <p>Bluetooth : dfasf</p>
-              <p>HDMI : dfasf</p>
-            </div>
+
+            {/* manggil function */}
+            {query3 && <>{filteredData3(productsSampleData, query3)}</>}
           </div>
         </div>
       </div>
