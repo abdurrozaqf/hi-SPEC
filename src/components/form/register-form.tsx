4d959d90ff092ee19c7ded3b43094f95db0034c4
Gerardusdavidbayuaji <gerardusdavidbayu@gmail.com>
2023-12-15 09:11:11 +0700

    feat: fix home page styling

--- src/components/form/register-form.tsx
+++ src/components/form/register-form.tsx
@@ -6,50 +6,83 @@ import { useToast } from "../ui/use-toast";
 import { RegisterAccount } from "@/utils/apis/auth";
 
 const RegisterForm = () => {
-    const { toast } = useToast();
+  const { toast } = useToast();
 
-    const [fullName, setFullName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [address, setAddress] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [address, setAddress] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
-    async function onSubmitRegister(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-            try {
-                const body = {
-                    full_name: fullName,
-                    email,
-                    password,
-                    address,
-                    phone_number: phoneNumber,
-                };
+  async function onSubmitRegister(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    try {
+      const body = {
+        full_name: fullName,
+        email,
+        password,
+        address,
+        phone_number: phoneNumber,
+      };
 
-                const result = await RegisterAccount(body)
-                    toast({
-                        description: result.message,
-            })
-            } catch (error: any) {
-            toast({
-                title: 'Oops, something went wrong!',
-                description: error.toString(),
-                variant: "destructive",
-            })
-        }
+      const result = await RegisterAccount(body);
+      toast({
+        description: result.message,
+      });
+    } catch (error: any) {
+      toast({
+        title: "Oops, something went wrong!",
+        description: error.toString(),
+        variant: "destructive",
+      });
     }
-    
+  }
+
   return (
-    <div>
-        <form className="flex flex-col w-full mx-auto gap-3 mt-10" onSubmit={(e) => onSubmitRegister(e)}>
-            <Input placeholder="Full Name" value={fullName} onChange={(e) => setFullName(e.target.value)}/>
-            <Input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-            <Input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <Input placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)}/>
-            <Input placeholder="Phone Number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
-            <Button type="submit">Register</Button>
-        </form>
+    <div className="text-[#1E1E1E] font-poppins">
+      Register your account now to get full access
+      <form
+        className="flex flex-col w-full mx-auto gap-3 mt-10"
+        onSubmit={(e) => onSubmitRegister(e)}
+      >
+        <p className="font-semibold">Full Name</p>
+        <Input
+          placeholder="Jhon Doe"
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+        />
+
+        <p className="font-semibold">Email</p>
+        <Input
+          placeholder="jhondoe@gmail.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <p className="font-semibold">Password</p>
+        <Input
+          placeholder="Minimum 8 characters"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <p className="font-semibold">Address</p>
+        <Input
+          placeholder="e.g: Jl. Veteran, Kec. Lowokwaru, Kota Malang, Jawa Timur"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+
+        <p className="font-semibold">Phone Number</p>
+        <Input
+          placeholder="e.g: +6281936273191"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        {/* <Button type="submit">Register</Button> */}
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default RegisterForm;
--- src/pages/auth/auth-layout.tsx
+++ src/pages/auth/auth-layout.tsx
@@ -1,64 +1,85 @@
 import Marquee from "react-fast-marquee";
 
-import { Separator } from "@/components/ui/separator"
-import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
-import CustomDialog from "@/components/Dialog"
+import CustomDialog from "@/components/Dialog";
+import RegisterForm from "@/components/form/register-form";
+import LoginForm from "@/components/form/login-form";
 
 const AuthLayout = () => {
-  
   function onSubmit() {}
 
   return (
     <div className="w-full h-screen font-poppins bg-[url('src/assets/Home-page-background.png')] bg-no-repeat flex flex-col">
-        <div className="container grid grid-cols-12 grow">
+      <div className="container grid grid-cols-12 grow">
+        {/* Bagian kiri */}
+        <div className="col-start-1 col-end-6 pt-12">
+          <h1 className="text-white font-poppins font-bold text-[45px]">
+            Smart solution <br /> for your brand are here
+          </h1>
+          <h3 className="text-white mt-16 font-semibold text-xl mb-2">
+            Join us now
+          </h3>
 
-          {/* Bagian kiri */}
-          <div className="col-start-1 col-end-6 pt-12">
-            <h1 className="text-white font-poppins font-bold text-[45px]">Smart solution <br /> for your brand are here</h1>
-            <h3 className="text-white pt-16 font-semibold text-xl mb-4">Join us now</h3>
-            
-            <div className="flex flex-col items-center justify-center">
-              <Button className="bg-[#C9DFF2] rounded-full w-[533px] h-[50px] text-[#1E1E1E] font-semibold text-base">
-                  Login
+          <div className="flex flex-col items-center justify-center">
+            <CustomDialog
+              title="Login"
+              description={<LoginForm />}
+              action="Login"
+              onAction={() => onSubmit}
+            >
+              <Button className="bg-[#C9DFF2] rounded-full w-[533px] h-[50px] text-[#1E1E1E] font-semibold text-base hover:text-white">
+                Login
               </Button>
+            </CustomDialog>
 
-              <Separator className="my-8 w-11/12"/>
-              <p className="bg-white w-[59px] text-[#1E1E1E] rounded-full font-medium text-xs text-center flex items-center justify-center -mt-10">
-                or
-              </p>
+            <Separator className="my-8 w-11/12" />
+            <p className="bg-white w-[59px] text-[#1E1E1E] rounded-full font-medium text-xs text-center flex items-center justify-center -mt-10">
+              or
+            </p>
 
-              <CustomDialog title="TEST" description={'TEST1'} action="Register" onAction ={ () => onSubmit}>
+            <CustomDialog
+              title="Register"
+              description={<RegisterForm />}
+              action="Register"
+              onAction={() => onSubmit}
+            >
               <Button className="bg-[#48B774] rounded-full w-[533px] h-[50px] mt-5 text-white font-semibold text-base">
-                  Register
+                Register
               </Button>
-              </CustomDialog>
-            </div>
-            
-            <div className="flex justify-between mt-4">
-              <p className="text-white font-semibold text-[12px]">
-                Enjoy exploring our products! Use the guest account <br /> to view the latest items
-              </p>
-              <Button className="bg-white h-[33px] rounded-full font-semibold text-[12px] text-[#1E1E1E]">
-                Try it view
-              </Button>
-            </div>
+            </CustomDialog>
           </div>
 
-          {/* Bagian kanan */}
-          <div className="col-start-7 col-end-13 relative">
-            <img src="src/assets/Home-page-human.png" alt="human" className="h-[560px] float-right absolute -bottom-1 right-0"/>
+          <div className="flex justify-between mt-4">
+            <p className="text-white font-semibold text-[12px]">
+              Enjoy exploring our products! Use the guest account <br /> to view
+              the latest items
+            </p>
+            <Button className="bg-white h-[33px] rounded-full font-semibold text-[12px] text-[#1E1E1E] hover:text-white">
+              Try it view
+            </Button>
           </div>
         </div>
 
-        {/* kelas footer */}
-        <footer className="bg-[#E4ECF1] w-full h-20 flex items-center justify-center z-10">
-          <Marquee pauseOnHover>
-            <img src="src/assets/Home-page-sponsor.png" alt="sponsor"/>
-          </Marquee>
-        </footer>
+        {/* Bagian kanan */}
+        <div className="col-start-7 col-end-13 relative">
+          <img
+            src="src/assets/Home-page-human.png"
+            alt="human"
+            className="h-[560px] float-right absolute -bottom-1 right-0 hover:"
+          />
+        </div>
+      </div>
+
+      {/* footer */}
+      <footer className="bg-[#E4ECF1] w-full h-20 flex items-center justify-center z-10">
+        <Marquee pauseOnHover direction="right">
+          <img src="src/assets/Home-page-sponsor.png" alt="sponsor" />
+        </Marquee>
+      </footer>
     </div>
-  )
-}
+  );
+};
 
 export default AuthLayout;
