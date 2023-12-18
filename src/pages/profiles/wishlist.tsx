9f304b624a5ade5a5ed0553ce3ebd82fc1b9c668
gufranlazuardi <gufranlazuardi@gmail.com>
2023-12-18 20:24:45 +0700

    feat: add wishlist page and types

--- src/pages/profiles/wishlist.tsx
+++ src/pages/profiles/wishlist.tsx
@@ -1,9 +1,46 @@
 import Layout from "@/components/Layout";
 import ProductCard from "@/components/ProductCard";
 import ProductCardWishlist from "@/components/ProductCardWishlist";
-import React from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { tokenUser } from "@/utils/apis/users";
+import { useToken } from "@/utils/contexts/token";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const WishList = () => {
+  const [wishlist, setWishlist] = useState<tokenUser>();
+  const navigate = useNavigate();
+  // const {token, user} = useToken()
+  const { toast } = useToast();
+  const params = useParams();
+
+  // async function handleAddWishlist(params:type) {
+  //   try {
+  //     const result = await addWishlist();
+  //     toast({ description: result.message });
+  //   } catch (error: any) {
+  //     toast({
+  //       title: "Oops! Something went wrong.",
+  //       description: error.toString(),
+  //       variant: "destructive",
+  //     });
+  //   }
+  // }
+
+  // async function handleDeleteWishlist(params:type) {
+  //   try {
+  //     const result = await deleteWishlist();
+
+  //     toast({ description: result.message });
+  //   } catch (error: any) {
+  //     toast({
+  //       title: "Oops! Something went wrong.",
+  //       description: error.toString(),
+  //       variant: "destructive",
+  //     });
+  //   }
+  // }
+
   return (
     <Layout>
       <div className="flex gap-4">
