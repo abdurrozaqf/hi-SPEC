13d80228fe3c7005f8c7e57f11f8f2076aee2c2f
abdurrozaqf <rozaqa27@gmail.com>
2023-12-24 18:03:02 +0700

    fix: responsive pages auth

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -13,61 +13,52 @@ const Login = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-full h-screen font-poppins background-login flex flex-col">
-      <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-around grow p-4 lg:p-0">
-        <div className="w-auto md:px-10">
-          <p className="text-white font-poppins font-bold text-3xl md:text-5xl">
+    <div className="w-full h-screen font-poppins background-login flex flex-col justify-between">
+      <div className="flex justify-center h-full">
+        <div className="flex flex-col justify-center gap-20 px-6 md:px-20">
+          <p className="text-2xl md:text-4xl lg:text-4xl xl:text-6xl font-bold">
             Smart solution <br /> for your brand are here
           </p>
-          <h3 className="text-white mt-10 lg:mt-16 font-semibold text-xl mb-2">
-            Join us now
-          </h3>
-
-          <div className="flex flex-col items-center justify-center">
-            <CustomDialog title="Login" description={<LoginForm />}>
-              <Button className="bg-[#C9DFF2] rounded-full w-[400px] md:w-[670px] lg:w-[500px] xl:w-[580px] py-6 lg:py-7 text-[#1E1E1E] font-semibold text-base hover:text-white">
+          <div className="flex flex-col gap-2 w-full">
+            <p className="text-xl xl:text-4xl font-semibold">Join us now</p>
+            <CustomDialog title="Login Account" description={<LoginForm />}>
+              <Button className="w-full h-fit py-3 text-lg rounded-full bg-[#C9DFF2] hover:bg-[#b0c8dc] text-black font-semibold tracking-wide">
                 Login
               </Button>
             </CustomDialog>
-
-            <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-white after:ml-4 after:block after:h-px after:flex-grow after:bg-white">
+            <div className="mx-auto px-2 my-2 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-white after:ml-4 after:block after:h-px after:flex-grow after:bg-white">
               <p className=" bg-white rounded-full px-6 py-1 leading-none text-black">
                 or
               </p>
             </div>
-
-            <CustomDialog title="Register" description={<RegisterForm />}>
-              <Button className="bg-[#48B774] rounded-full w-[400px] md:w-[670px] lg:w-[500px] xl:w-[580px] py-6 lg:py-7 text-white font-semibold text-base">
+            <CustomDialog
+              title="Register Account"
+              description={<RegisterForm />}
+            >
+              <Button className="w-full h-fit py-3 text-lg rounded-full bg-[#48B774] hover:bg-[#3b9e62] text-white font-semibold tracking-wide">
                 Register
               </Button>
             </CustomDialog>
-          </div>
-
-          <div className="flex justify-between mt-4">
-            <p className="text-white font-semibold text-[12px] w-[240px] md:w-[330px]">
-              Enjoy exploring our products! Use the guest account to view the
-              latest items
-            </p>
-            <Button
-              onClick={() => navigate("/")}
-              className="bg-white h-[33px] rounded-full font-semibold text-[12px] text-[#1E1E1E] hover:text-white"
-            >
-              Try it view
-            </Button>
+            <div className="flex items-center justify-between">
+              <p className="text-xs md:text-base font-medium pr-3 md:pr-10 xl:w-[500px]">
+                Enjoy exploring our products! Use the guest account to view the
+                latest items
+              </p>
+              <Button
+                onClick={() => navigate("/")}
+                className="rounded-full h-fit bg-white text-black"
+              >
+                try it view
+              </Button>
+            </div>
           </div>
         </div>
-
-        <div className="h-full lg:flex items-end -mb-2 hidden absolute lg:relative">
-          <img
-            src={HumanLogin}
-            alt="human"
-            className="h-[560px] xl:h-[650px] w-auto"
-            loading="lazy"
-          />
+        <div className="hidden lg:flex items-end -mb-4">
+          <img src={HumanLogin} alt="human" className="object-cover" />
         </div>
       </div>
 
-      <footer className="bg-[#E4ECF1] w-0 lg:w-full h-0 lg:h-20 flex items-center justify-center z-10">
+      <footer className="bg-[#E4ECF1] hidden lg:flex items-center justify-center h-20">
         <Marquee pauseOnHover direction="right">
           <img src={LinesSponsor} alt="sponsors" className="flex mr-20" />
         </Marquee>
