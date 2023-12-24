808a0e0d6a2bc4f24e5d0e8a0378dcf4299efbb4
abdurrozaqf <rozaqa27@gmail.com>
2023-12-24 18:04:13 +0700

    fix: font color auth page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -16,11 +16,13 @@ const Login = () => {
     <div className="w-full h-screen font-poppins background-login flex flex-col justify-between">
       <div className="flex justify-center h-full">
         <div className="flex flex-col justify-center gap-20 px-6 md:px-20">
-          <p className="text-2xl md:text-4xl lg:text-4xl xl:text-6xl font-bold">
+          <p className="text-2xl md:text-4xl lg:text-4xl xl:text-6xl font-bold text-white">
             Smart solution <br /> for your brand are here
           </p>
           <div className="flex flex-col gap-2 w-full">
-            <p className="text-xl xl:text-4xl font-semibold">Join us now</p>
+            <p className="text-xl xl:text-4xl font-semibold text-white">
+              Join us now
+            </p>
             <CustomDialog title="Login Account" description={<LoginForm />}>
               <Button className="w-full h-fit py-3 text-lg rounded-full bg-[#C9DFF2] hover:bg-[#b0c8dc] text-black font-semibold tracking-wide">
                 Login
@@ -40,7 +42,7 @@ const Login = () => {
               </Button>
             </CustomDialog>
             <div className="flex items-center justify-between">
-              <p className="text-xs md:text-base font-medium pr-3 md:pr-10 xl:w-[500px]">
+              <p className="text-xs md:text-base font-medium pr-3 md:pr-10 xl:w-[500px] text-white">
                 Enjoy exploring our products! Use the guest account to view the
                 latest items
               </p>
